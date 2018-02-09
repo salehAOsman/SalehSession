711cd1a81e368c0f6b9daf18658d1a408a3037f2
Saleh Osman <saleh1.a.osman@gmail.com>
2018-02-09 23:57:38 +0100

    Add project files.

---
+++ DisplayLIstBySession/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using DisplayLIstBySession.Models;//to pass  "Book" collection from Class as input to work with it
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DisplayLIstBySession.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()//beging method
+        {
+            var peopleCollecion = new List<Person>();                                //reference for list type generic or object                                                 
+            if (Session["PeopleCollecion"] != null)
+            {
+                peopleCollecion = (List<Person>)Session["PeopleCollecion"];           //casting session as list type the assign it to object of list same type  
+            }
+            return View(peopleCollecion);
+        }
+
+        [HttpGet]
+        public ActionResult AddPerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPerson(Person newBook)                                    //We need to sumit form from view "AddBook"
+        {
+            List<Person> peopleCollecion = new List<Person>();                           //we need object to deal with this class and this to be as list of book//reference for list type generic or object
+            if (Session["PeopleCollecion"] !=null)
+            {
+                peopleCollecion = (List<Person>)Session["PeopleCollecion"];
+            }
+            peopleCollecion.Add(newBook);                                                //I am going to persist that information to the session//We call this session "BooksCollecion" 
+            Session["PeopleCollecion"] = peopleCollecion;                                //if booksCollection is not equal to null then I want the current books so books collection dot add then                                  
+
+            if (ModelState.IsValid)                                                      // then I update the session I is going to equal the current collection of books so 
+            {
+            return RedirectToAction("Index");                                            // here istead of returning a view we will return by Redirection to action "index" 
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DisplayLIstBySession/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DisplayLIstBySession.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "your name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Phone")]
+        [Required(ErrorMessage = "your phone")]
+        public string Phone { get; set; }
+
+        [Display(Name = "City")]
+        [Required(ErrorMessage = " your city")]
+        public string City { get; set; }
+        //[DataType(DataType.Date)]
+        //public DateTime PublishDate { get; set; }
+    }
+}
